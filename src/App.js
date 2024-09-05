0f7e40165cd9eb1662b6c36240cf73e42d217689
debanjan <debanjan@pitangent.com>
2024-09-05 19:06:54 +0530

    completed nested comment

--- src/App.js
+++ src/App.js
@@ -1,23 +1,64 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+import Comment from "./components/Comment";
+import { useState } from "react";
+import useNode from "./hooks/useNode";
+const comments = {
+  id: 1,
+  items: [
+    // {
+    //   id: 8464784884,
+    //   name: "hello",
+    //   items: [
+    //     {
+    //       id: 3883893993,
+    //       name: "hello world",
+    //       items: [
+    //         {
+    //           id: 78667878,
+    //           name: "hello hello hello world",
+    //           items: [],
+    //         },
+    //       ],
+    //     },
+    //   ],
+    // },
+    // {
+    //   id: 98889444,
+    //   name: " 2nd hello",
+    //   items: [],
+    // },
+  ],
+};
 
 function App() {
+  const [commentsData, setCommentsData] = useState(comments);
+
+  const { insertNode, editNode, deleteNode } = useNode();
+
+  const handleInsertNode = (folderId, item) => {
+    const finalStructure = insertNode(commentsData, folderId, item);
+    setCommentsData(finalStructure);
+  };
+
+  const handleEditNode = (folderId, value) => {
+    const finalStructure = editNode(commentsData, folderId, value);
+    setCommentsData(finalStructure);
+  };
+
+  const handleDeleteNode = (folderId) => {
+    const finalStructure = deleteNode(commentsData, folderId);
+    const temp = { ...finalStructure };
+    setCommentsData(temp);
+  };
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Comment
+        handleInsertNode={handleInsertNode}
+        handleEditNode={handleEditNode}
+        handleDeleteNode={handleDeleteNode}
+        comment={commentsData}
+      />
     </div>
   );
 }
